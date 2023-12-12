958aeab0bfa5287cc290c988c0181787a1989cbf
danielbatres <danielbatresuser@gmail.com>
2023-12-12 09:28:51 -0600

    Add base containers

---
+++ src/Containers/BaseLayout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useMachine } from "@xstate/react";
+import { Nav } from "../Components/Nav";
+import { StepsLayout } from "./StepsLayout";
+import bookingMachine from "../Machines/bookingMachine";
+import "./BaseLayout.css";
+
+const BaseLayout = () => {
+  const [state, send] = useMachine(bookingMachine);
+
+  console.log("machine", state.value);
+
+  return (
+    <div className="BaseLayout">
+      <Nav state={state} send={send} />
+      <StepsLayout state={state} send={send} />
+    </div>
+  );
+};
+
+export { BaseLayout }
